6347c94e3fd4c78acbe6f4c7b40239ce09c75a62
Scott Barbour <smbarbour@gmail.com>
2014-01-28 08:07:56 -0600

    Work on login dialog

--- src/org/mcupdater/gui/LoginDialog.java
+++ src/org/mcupdater/gui/LoginDialog.java
@@ -1,7 +1,48 @@
 package org.mcupdater.gui;
 
-/**
- * Created by sbarbour on 1/26/14.
- */
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.mcupdater.settings.Profile;
+
+import java.io.IOException;
+
 public class LoginDialog {
+    private static Profile newProfile;
+    public Label lblUser;
+    public Label lblPassword;
+    public TextField txtUser;
+    public PasswordField txtPassword;
+    public Button btnOK;
+    public Button btnCancel;
+
+    public static Profile doLogin(Stage parent, String initialUsername) {
+        newProfile = new Profile();
+        newProfile.setStyle("Invalid");
+        Stage dialog = new Stage(StageStyle.UTILITY);
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.initOwner(parent);
+        try {
+            FXMLLoader loader = new FXMLLoader(LoginDialog.class.getResource("LoginDialog.fxml"));
+            Parent root = (Parent) loader.load();
+            Scene scene = new Scene(root);
+            dialog.setTitle("Login");
+            dialog.setScene(scene);
+            LoginDialog controller = loader.getController();
+            controller.txtUser.setText(initialUsername);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        dialog.showAndWait();
+        return newProfile;
+    }
 }
