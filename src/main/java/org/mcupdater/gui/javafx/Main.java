5f44c537da19db4dd496739713db1d0d3361e6d8
Ammon Lauritzen <allaryin@gmail.com>
2018-03-08 15:07:17 -0600

    Got FXML+CSS basics working.

--- src/main/java/org/mcupdater/gui/javafx/Main.java
+++ src/main/java/org/mcupdater/gui/javafx/Main.java
@@ -3,24 +3,28 @@ package org.mcupdater.gui.javafx;
 import org.mcupdater.api.Version;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO: switch to FXML
-		StackPane root = new StackPane();
+		Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
 		
 		Scene scene = new Scene(root, 1175, 600);
 		
 		stage.setTitle("MCUpdater "+Version.VERSION);
 		stage.setScene(scene);
-		stage.getIcons().add(new Image(Main.class.getResourceAsStream("mcu-icon.png")));
+		stage.getIcons().add(getImage("icons/mcu-icon.png"));
 		stage.show();
+	}
+	
+	public Image getImage(String filename) {
+		return new Image(getClass().getResourceAsStream(filename));
 	}
 
 	public static void main(String[] args) {
